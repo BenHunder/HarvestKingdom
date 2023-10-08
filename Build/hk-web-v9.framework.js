c205d1ff30fc2e82e87102d15c277a71808fa3ab
Ben Hunder <benhunder@gmail.com>
2023-10-08 01:36:03 -0500

    v9ish

--- Build/hk-web-v9.framework.js
+++ Build/hk-web-v9.framework.js
@@ -1303,7 +1303,7 @@ function _emscripten_asm_const_id(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 3196672;
+STATICTOP = STATIC_BASE + 3197488;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AIScriptingClasses_cpp();
@@ -3377,7 +3377,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 3196672;
+var STATIC_BUMP = 3197488;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
